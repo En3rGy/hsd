c05b8d0c7bf11477860ec9c6d25f3eab7b89d3da
En3rGy <en3rgy@web.de>
2013-05-24 06:45:01 +0200

    added german translations

--- src/hsd_de.ts
+++ src/hsd_de.ts
@@ -2,155 +2,195 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="de_DE">
 <context>
+    <name>CTcpClient</name>
+    <message>
+        <location filename="tcpclient.cpp" line="153"/>
+        <source>Web request closed</source>
+        <translation>Web-Abfrage geschlossen</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="186"/>
+        <source>Disconnected from Homeserver.</source>
+        <translation>Verbindung zum Homeserver getrennt.</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="193"/>
+        <source>Trying to reconnect to HS.</source>
+        <translation>Versuche, Verbindung zum HS wieder herzustellen.</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="328"/>
+        <source>Wrote to HS</source>
+        <translation>Daten gesendet</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="369"/>
+        <source>Send</source>
+        <translation>Sendete</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="369"/>
+        <source>byte:</source>
+        <translation>Byte:</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="369"/>
+        <location filename="tcpclient.cpp" line="373"/>
+        <source>to</source>
+        <translation>zu</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="373"/>
+        <source>while trying to send</source>
+        <translation>während des Sendens</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="378"/>
+        <source>while trying to connect to</source>
+        <translation>während des Verbindungsaufbaus zu</translation>
+    </message>
+</context>
+<context>
     <name>QObject</name>
     <message>
-        <location filename="eibdmsg.cpp" line="21"/>
+        <location filename="eibdmsg.cpp" line="22"/>
         <source>Received too short message</source>
         <translation>Empfangene Nachricht ist zu kurz</translation>
     </message>
     <message>
-        <location filename="eibdmsg.cpp" line="102"/>
+        <location filename="eibdmsg.cpp" line="108"/>
         <source>Received unknown message:</source>
         <translation>Unbekannte Nachricht empfangen:</translation>
     </message>
     <message>
-        <location filename="eibdmsg.cpp" line="224"/>
+        <location filename="eibdmsg.cpp" line="230"/>
         <source>Can only forward positive natural numbers &lt; 100, not</source>
         <translation>Nur natürliche Zahlen &lt; 100 können übermittelt werden</translation>
     </message>
     <message>
-        <location filename="groupaddress.cpp" line="114"/>
+        <location filename="groupaddress.cpp" line="115"/>
         <source>not of kind a/b/c. Aborting.</source>
         <translation>entspricht nicht a/b/c. Abbruch.</translation>
     </message>
     <message>
-        <location filename="koxml.cpp" line="54"/>
+        <location filename="koxml.cpp" line="56"/>
         <source>Received</source>
         <translation>Empfangen</translation>
     </message>
     <message>
-        <location filename="koxml.cpp" line="54"/>
+        <location filename="koxml.cpp" line="56"/>
         <source>elements from HS.</source>
         <translation>Elemente vom HS.</translation>
     </message>
     <message>
-        <location filename="koxml.cpp" line="99"/>
+        <location filename="koxml.cpp" line="101"/>
         <source>Unknown</source>
         <translation>Unbekannt</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="32"/>
         <source>hsd provides the eibd TCP/IP interface to access the KNX bus via the GIRA Homeserver KO-Gateway.
 </source>
-        <translation>hsd stellt die eibd TCP/IP Schnittstelle zur Verfügung, um den KNX Bus über das GIRA Homeserver KO_Gateway anzusprechen. </translation>
+        <translation type="obsolete">hsd stellt die eibd TCP/IP Schnittstelle zur Verfügung, um den KNX Bus über das GIRA Homeserver KO_Gateway anzusprechen. </translation>
     </message>
     <message>
-        <location filename="main.cpp" line="33"/>
         <source>
 Configure settings in [hsd]/etc/hsd.ini.</source>
-        <translation>Einstellungen ändern in: [hsd]/etc/hsd.ini.</translation>
+        <translation type="obsolete">Einstellungen ändern in: [hsd]/etc/hsd.ini.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="34"/>
         <source>
 Log file is written to [hsd]/var/hsd.log
 </source>
-        <translation>Log-Datei wird gespeicher in: [hsd]/var/hsd.log</translation>
+        <translation type="obsolete">Log-Datei wird gespeicher in: [hsd]/var/hsd.log</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="36"/>
         <source>
 Settings file options:</source>
-        <translation>Optionen in der Einstellungs-Datei:</translation>
+        <translation type="obsolete">Optionen in der Einstellungs-Datei:</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="37"/>
         <source>	Port, where hsd listens for eibd messages.</source>
-        <translation>Port, auf dem hsd nach eibd Nachrichten lauscht.</translation>
+        <translation type="obsolete">Port, auf dem hsd nach eibd Nachrichten lauscht.</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="38"/>
         <source>	GIRA Homeserver KO-Gateway port</source>
-        <translation>GIRA Homeserver KO-Gateway Port</translation>
+        <translation type="obsolete">GIRA Homeserver KO-Gateway Port</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="39"/>
         <source>	IP address of GIRA Homeserver</source>
-        <translation>IP-Adresse des GIRA Homeserver</translation>
+        <translation type="obsolete">IP-Adresse des GIRA Homeserver</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="40"/>
         <source>	Port of GIRA Homeserver web server</source>
-        <translation>GIRA Homeserver Web-Server Port</translation>
+        <translation type="obsolete">GIRA Homeserver Web-Server Port</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="41"/>
         <source>	Hsd log level (see below)</source>
-        <translation>Hsd Log-Level (s. unten)</translation>
+        <translation type="obsolete">Hsd Log-Level (s. unten)</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="43"/>
         <source>
 Command line options:
 </source>
-        <translation>Kommandozeilenoptionen:</translation>
+        <translation type="obsolete">Kommandozeilenoptionen:</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="45"/>
         <source>
 --?	This help page</source>
-        <translation>--?	Diese Hilfe-Seite</translation>
+        <translation type="obsolete">--?	Diese Hilfe-Seite</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="46"/>
         <source>
 -lx	x=0 (TraceLevel),</source>
-        <translation>-lx	x=0 (Trace Level)</translation>
+        <translation type="obsolete">-lx	x=0 (Trace Level)</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="47"/>
         <source>
 	 =1 (DebugLevel),</source>
-        <translation>	 =1 (Debug Level),</translation>
+        <translation type="obsolete">	 =1 (Debug Level),</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="48"/>
         <source>
 	 =2 (InfoLevel),</source>
-        <translation>	 =2 (Info Level),</translation>
+        <translation type="obsolete">	 =2 (Info Level),</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="49"/>
         <source>
 	 =3 (WarnLevel),</source>
-        <translation>	 =3 (Warn-Level),</translation>
+        <translation type="obsolete">	 =3 (Warn-Level),</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="50"/>
         <source>
 	 =4 (ErrorLevel, default),</source>
-        <translation>	 =4 (Error-Level, Standard),</translation>
+        <translation type="obsolete">	 =4 (Error-Level, Standard),</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="51"/>
         <source>
 	 =5 (FatalLevel)</source>
-        <translation>	 =5 (Schwerer Fehler-Level)</translation>
+        <translation type="obsolete">	 =5 (Schwerer Fehler-Level)</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="89"/>
         <source>Program starting. Use &quot;hsd --?&quot; for help</source>
-        <translation>Programm startet. &quot;hsd --?&quot; für Hilfe</translation>
+        <translation type="obsolete">Programm startet. &quot;hsd --?&quot; für Hilfe</translation>
     </message>
     <message>
-        <location filename="tcpclient.cpp" line="58"/>
+        <location filename="tcpclient.cpp" line="53"/>
         <source>Connection with HS not yet initialized.</source>
         <translation>Verbindung mit Hs noch nicht hergestellt.</translation>
     </message>
     <message>
-        <location filename="tcpclient.cpp" line="64"/>
         <source>Not connected with HS</source>
-        <translation>Keine Verbindung mit HS</translation>
+        <translation type="obsolete">Keine Verbindung mit HS</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="59"/>
+        <source>Not connected with HS.</source>
+        <translation>Keine Verbindung zum HS.</translation>
+    </message>
+    <message>
+        <location filename="tcpclient.cpp" line="63"/>
+        <source>Connection could not re-establish. Discarding message.</source>
+        <translation>Verbindung konnte nicht wieder aufgebaut werden. Nachricht verworfen.</translation>
     </message>
     <message>
         <location filename="tcpclient.cpp" line="86"/>
@@ -184,64 +224,174 @@ Command line options:
         <translation>Wert:</translation>
     </message>
     <message>
-        <location filename="tcpclient.cpp" line="155"/>
+        <location filename="tcpclient.cpp" line="156"/>
         <source>Connection to peer closed:</source>
         <translation>Verbindung zur gegenstelle getrennt:</translation>
     </message>
     <message>
-        <location filename="tcpclient.cpp" line="240"/>
+        <location filename="tcpclient.cpp" line="255"/>
         <source>Connection with HS established</source>
         <translation>Verbindung mit dem HS aufgebaut</translation>
     </message>
     <message>
-        <location filename="tcpclient.cpp" line="246"/>
+        <location filename="tcpclient.cpp" line="265"/>
         <source>Sending to HS: Initialization message.</source>
         <translation>Zum HS gesendet: Initialisierungs-Nachricht.</translation>
     </message>
     <message>
-        <location filename="tcpclient.cpp" line="300"/>
+        <location filename="tcpclient.cpp" line="317"/>
         <source>Already a HS web request ongoing</source>
         <translation>Es besteht bereits eine Verbindung zum HS</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="107"/>
+        <location filename="tcpserver.cpp" line="106"/>
         <source>Received via eibd interface: Connection request. Granted.</source>
         <translation>Via eibd Schnittstelle empfangen: Verbindungsanfrage. Stattgegeben.</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="114"/>
+        <location filename="tcpserver.cpp" line="113"/>
         <source>Received via eibd interface: openGroupSocket request. Granted.</source>
         <translation>Via eibd Schnittstelle empfangen: GroupSocket öffnen Anfrage. Stattgegeben.</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="121"/>
+        <location filename="tcpserver.cpp" line="120"/>
         <source>Received via eibd interface: simpleWrite request</source>
         <translation>Via eibd Schnittstelle empfangen: Einfache Schreib-Anfrage</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="121"/>
+        <location filename="tcpserver.cpp" line="120"/>
         <source>. Forwarded.</source>
         <translation>. Weitergeleitet.</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="129"/>
+        <location filename="tcpserver.cpp" line="128"/>
+        <source>Received via eibd interface: message size</source>
+        <translation>Über eibd Schnittstelle empfangen: Nachrichten Größe</translation>
+    </message>
+    <message>
+        <location filename="tcpserver.cpp" line="137"/>
+        <source>Reveived EXIT programm message via eibd interface. Shutting down.</source>
+        <translation>EXIT Programm-Nachricht via eibd Schnittstelle empfangen. Programm wird beendet.</translation>
+    </message>
+    <message>
+        <location filename="tcpserver.cpp" line="142"/>
         <source>Received via eibd interface: Unknown request:</source>
         <translation>Via eibd schnittstelle empfangen: Unbekannte Anfrage:</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="137"/>
+        <location filename="tcpserver.cpp" line="156"/>
         <source>Disconnected from eibd client</source>
         <translation>Vom eibd Client getrennt</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="161"/>
+        <location filename="tcpserver.cpp" line="180"/>
         <source>No eibd client connected to hsd server. Discarding incomming EIB/KNX update.</source>
         <translation>Kein eibd Client verbunden. Eingehende EIB/KNX Aktualisierung wurde verworfen.</translation>
     </message>
     <message>
-        <location filename="tcpserver.cpp" line="165"/>
+        <location filename="tcpserver.cpp" line="184"/>
         <source>Sending via eibd interface</source>
         <translation>Via eibd Schnittstelle gesendet</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="21"/>
+        <source>hsd provides the eibd TCP/IP interface to access the KNX bus via the GIRA Homeserver KO-Gateway.</source>
+        <translation>hsd stellt die eib TCP/IP Schnittstelle für den KNX Bus Zugriff über das GIRA Homeserver KO-Gateway zur Verfügung.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="22"/>
+        <source>Configure settings in [hsd]/etc/hsd.ini.</source>
+        <translation>Einstellungen in [hsd]/etc/hsd.ini bearbeiten.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="23"/>
+        <source>Log file is written to [hsd]/var/hsd.log</source>
+        <translation>Log Dateien werden unter [hsd]/var(hsd.log gespeichert </translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="25"/>
+        <source>Settings file options:</source>
+        <translation>Optionen der Einstellungsdatei:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="26"/>
+        <source>Port, where hsd listens for eibd messages.</source>
+        <translation>Port, auf dem hsd nach eibd Nachrichten lauscht.</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="27"/>
+        <source>GIRA Homeserver KO-Gateway port</source>
+        <translation>Port des GIRA HOmeservers KO-Gateways</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="28"/>
+        <source>IP address of GIRA Homeserver</source>
+        <translation>IP Adresse des GIRA Homeserver</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="29"/>
+        <source>Port of GIRA Homeserver web server</source>
+        <translation>Port des GIRA Homeserver Webservers</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="30"/>
+        <source>Hsd log level (see below)</source>
+        <translation>Hsd Log-Level (s. unten)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="32"/>
+        <source>Command line options:</source>
+        <translation>Kommandozeilenoptionen:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="34"/>
+        <source>This help page</source>
+        <translation>Diese Hilfe-Seite</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="35"/>
+        <source>Setting the log level x=0 (TraceLevel),</source>
+        <translation>Log-Level vorgeben x=0 (TraceLevel),</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="36"/>
+        <source>=1 (DebugLevel),</source>
+        <translation>=1 (DebugLevel),</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="37"/>
+        <source>=2 (InfoLevel),</source>
+        <translation>=2 (InfoLevel),</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="38"/>
+        <source>=3 (WarnLevel),</source>
+        <translation>=3 (WarnLevel),</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="39"/>
+        <source>=4 (ErrorLevel, default),</source>
+        <translation>=4 (ErrorLevel, Standard)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="40"/>
+        <source>=5 (FatalLevel)</source>
+        <translation>=5 (FatalLevel)</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="41"/>
+        <source>Printing program version</source>
+        <translation>Programm Version ausgeben</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="42"/>
+        <source>Printing adress convertion, e.g. hsd -c 4200 returns:</source>
+        <translation>Adresskonvertierung ausgeben, z.B. hsd -c 4200 liefert:</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="44"/>
+        <source>Exit running hsd instances.</source>
+        <translation>Beendet laufende hsd Instanzen.</translation>
     </message>
 </context>
 </TS>
