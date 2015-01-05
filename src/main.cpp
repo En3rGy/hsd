ad954aa13eed1d511fba7dab7dbf90c69834b0c7
En3rGy <en3rgy@web.de>
2015-01-05 19:06:35 +0100

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
         QCoreApplication a(argc, argv);
         a.setOrganizationName( "PImp" );
         a.setApplicationName( "hsd" );
-        a.setApplicationVersion( "0.4.10" );
+        a.setApplicationVersion( "0.4.8" );
 
         QString sLocale = QLocale::system().name();
 
@@ -142,17 +142,17 @@ int main(int argc, char *argv[])
             bValidArg = true;
         }
 
-        if ( bValidArg == false )
+
+         if ( bValidArg == false )
         {
             printHelpPage();
             return EXIT_SUCCESS;
         }
 
-        CHsd grHsd;
+       CHsd grHsd;
 
         if ( grArgsList.contains( "-E" ) == true )
         {
-            bValidArg = true;
             grHsd.stopService();
             return EXIT_SUCCESS;
         }
